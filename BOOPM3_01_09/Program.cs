d2a76c5c3ac120ee57ea69b1bd0613d80a119823
Martin Lenart <martin_lenart@icloud.com>
2021-11-16 17:16:31 +0100

    BOOPM3_01_09 updated

--- BOOPM3_01_09/Program.cs
+++ BOOPM3_01_09/Program.cs
@@ -6,21 +6,18 @@ namespace BOOPM3_01_09
     {
         public static class FooClass
         {
-            public static string Foo(int x) => "int";
-            public static string Foo(double x) => "double";
-            public static string Foo(int x, float y) => "int, float";
-            public static string Foo(float x, int y) => "float, int";
-
-            //Illegal overloading, so compiler error
-            //public static int Foo(int x) => x;
-            //public static (float, int) Foo(float x, int y) => (x, y);
+            //For overloading the signature is name and parameter types (in order).
+            //I call it OL-signature
+            //Methods can be overloaded if no OL-signature conflict exist
+            public static string MetodA(int x, double y) => "int and double";
+            public static double MetodA(double x, int y) => x;          // OK - no OL-signature conflict 
+            public static double MetodA(double x) => x;                 // OK - no OL-signature conflict 
+            public static string MetodA(double x) => "double";          // Not OK - signature conflict, compiler error 
+            public static string MetodA(double x, bool b) => "double";  // OK - no OL-signature conflict  
+            public static bool MetodA(double x, bool b) => b;           // Not OK - signature conflict, compiler error  
         }
         static void Main(string[] args)
         {
-            Console.WriteLine(FooClass.Foo(123));       // int
-            Console.WriteLine(FooClass.Foo(123.0));     // double
-            Console.WriteLine(FooClass.Foo(123, 123F)); // int, float
-            Console.WriteLine(FooClass.Foo(123F, 123));	// float, int
         }
     }
 
